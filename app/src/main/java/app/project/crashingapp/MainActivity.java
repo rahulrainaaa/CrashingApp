dd61f88d89622e6c389741b89e042b3c7f838e38
rahulrainahomeee <rahulrainahome@gmail.com>
2017-02-03 17:34:20 +0530

    Initial Commit.

--- app/src/main/java/app/project/crashingapp/MainActivity.java
+++ app/src/main/java/app/project/crashingapp/MainActivity.java
@@ -6,21 +6,33 @@ import android.view.View;
 import android.widget.Button;
 
 import com.crashlytics.android.Crashlytics;
+import com.digits.sdk.android.Digits;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TWITTER_KEY = "--------------";
+    private static final String TWITTER_SECRET = "---------------";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits.Builder().build());
+
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        //TrackingID:- UA-91366549-1
+
 
         Fabric.with(this, new Crashlytics());
 
         Button b = new Button(this);
+        b.setText("click button ");
         setContentView(b);
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
