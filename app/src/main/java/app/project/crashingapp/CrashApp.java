57ccb9104c6cf9b6e2122df45927f23718087270
rahulrainahomeee <rahulrainahome@gmail.com>
2017-02-03 17:23:50 +0530

    Initial commit

---
+++ app/src/main/java/app/project/crashingapp/CrashApp.java
@@ -0,0 +1,11 @@
+package app.project.crashingapp;
+
+import android.app.Application;
+
+
+public class CrashApp extends Application {
+
+    private final String PROPERTY_ID = "UA-91366549-1";
+
+
+}
---
+++ app/src/main/java/app/project/crashingapp/MainActivity.java
@@ -0,0 +1,36 @@
+package app.project.crashingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //TrackingID:- UA-91366549-1
+
+        Fabric.with(this, new Crashlytics());
+
+        Button b = new Button(this);
+        setContentView(b);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                TextView t = null;
+                t.setText("");
+
+            }
+        });
+    }
+}
