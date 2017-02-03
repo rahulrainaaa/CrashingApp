252d31f311b1a4d42ae758bfe5611c02154e8dc7
rahulrainahomeee <rahulrainahome@gmail.com>
2017-02-03 17:25:12 +0530

    Initial Commit.

--- app/src/main/java/app/project/crashingapp/MainActivity.java
+++ app/src/main/java/app/project/crashingapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 
@@ -26,9 +25,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                //Write some app crashing code.
 
-                TextView t = null;
-                t.setText("");
 
             }
         });
